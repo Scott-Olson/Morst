9c2444d6a0ba4788766be4b7b29f46bd7d7977d6
scott-olson <scotteolson3@gmail.com>
2020-08-24 12:58:08 -0700

    update project name; RustMorse -> Morst

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ use mors::{decoder, encoder};
 
 fn main() {
     println!(" -=-=-=-=-=-=- Main Loop -=-=-=-=-=-=- ");
-    let s = String::from("test").to_uppercase();
+    let s = String::from("morst").to_uppercase();
 
     let morse_encoder = encoder::Encoder::new();
     let result_encode = morse_encoder.encode_letters(s);
 
     let decoder = decoder::Decoder::new();
-    let message = String::from(". - --- -.-");
+    let message = String::from("-- --- .-. ... -");
     let result_decode = decoder.decode_message(message);
     println!("Encode: {0}, Decode: {1} ", result_encode, result_decode);
 }
